afda6ef61027b8e8214ce6945934b0117311e25c
Rampagerushtc <rampagerushtc@gmail.com>
2019-10-26 17:44:40 +0500

    final css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from '../pages';
+import ShopHeader from '../header';
 
 
 import './app.css';
@@ -10,6 +11,7 @@ const App = ({ bookstoreService }) => {
     console.log(bookstoreService.getBooks());
     return (
         <main role="main" className="container">
+            <ShopHeader numItems={5} total={200} />
             <Switch>
                 <Route path="/" exact component={HomePage} />
                 <Route path="/cart/:id" component={CartPage} />
